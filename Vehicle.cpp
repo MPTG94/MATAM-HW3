092887c457baa175ae79b16167c5fb4a8f223282
Sivan Yosebashvili <sivany100@gmail.com>
2020-01-09 19:24:50 +0200

    add defines for all the prices calculates

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -1,6 +1,14 @@
 //
 // Created by Mor on 09/01/2020.
 //
+#define PRICE_FOR_HANDICAPPED 15
+#define PRICE_FOR_FIRST_HOUR_MOTORBIKE 10
+#define PRICE_FOR_EXTRA_HOURS_MOTORBIKE 5
+#define MAX_PRICE_FOR_MOTORBIKE 35
+#define PRICE_FOR_FIRST_HOUR_CAR 20
+#define PRICE_FOR_EXTRA_HOURS_CAR 10
+#define MAX_PRICE_FOR_CAR 70
+
 
 #include "Vehicle.h"
 
@@ -22,27 +30,27 @@ unsigned int Vehicle::calculateParkingPrice(Time exitTime) {
     unsigned int totalTimeInHours = totalTime.toHours();
 
     if (CurrentType == HANDICAPPED) {
-        price = 15;
+        price = PRICE_FOR_HANDICAPPED;
     } else if (CurrentType == MOTORBIKE) {
         if (totalTimeInHours <= oneHour) {
-            price =10;
+            price =PRICE_FOR_FIRST_HOUR_MOTORBIKE;
         }
         else if ((totalTimeInHours > oneHour)&&(totalTimeInHours<=6*oneHour)) {
-            price= 10 + 5 * (totalTimeInHours - 1);
+            price= PRICE_FOR_FIRST_HOUR_MOTORBIKE + PRICE_FOR_EXTRA_HOURS_MOTORBIKE * (totalTimeInHours - 1);
         }
         else{
-            price = 35;
+            price = MAX_PRICE_FOR_MOTORBIKE;
         }
     }
     else {
         if (totalTimeInHours <= oneHour) {
-            price =20;
+            price =PRICE_FOR_FIRST_HOUR_CAR;
         }
         else if ((totalTimeInHours > oneHour)&&(totalTimeInHours<=6*oneHour)) {
-            price= 20 + 10 * (totalTimeInHours - 1);
+            price= PRICE_FOR_FIRST_HOUR_CAR + PRICE_FOR_EXTRA_HOURS_CAR * (totalTimeInHours - 1);
         }
         else{
-            price = 70;
+            price = MAX_PRICE_FOR_CAR;
         }
     }
     return price;
