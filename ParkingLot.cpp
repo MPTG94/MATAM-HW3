498c21a20a351041c66d6b35ed5f37634c447ca9
Mor Paz <1912.mor@gmail.com>
2020-01-10 19:50:11 +0200

    remove some unnecessary documentation

--- ParkingLot.cpp
+++ ParkingLot.cpp
@@ -7,28 +7,14 @@
 
 using namespace MtmParkingLot;
 
-/**
- * Construct a parking lot object
- * @param parkingBlockSizes
- */
 MtmParkingLot::ParkingLot::ParkingLot(unsigned int *parkingBlockSizes)
         : motorbikeArray(parkingBlockSizes[0]),
           handicapArray(parkingBlockSizes[1]),
           carArray(parkingBlockSizes[2]) {
 }
 
-/**
- * Destroy parking lot object
- */
 MtmParkingLot::ParkingLot::~ParkingLot() = default;
 
-/**
- * Register a vehicle in the parking lot
- * @param vehicleType
- * @param licensePlate
- * @param entranceTime
- * @return
- */
 ParkingLotUtils::ParkingResult MtmParkingLot::ParkingLot::enterParking(
         ParkingLotUtils::VehicleType vehicleType,
         ParkingLotUtils::LicensePlate licensePlate,
@@ -36,24 +22,12 @@ ParkingLotUtils::ParkingResult MtmParkingLot::ParkingLot::enterParking(
     return VEHICLE_ALREADY_PARKED;
 }
 
-/**
- * Get the place a vehicle is parked at
- * @param licensePlate
- * @param parkingSpot
- * @return
- */
 ParkingLotUtils::ParkingResult MtmParkingLot::ParkingLot::getParkingSpot(
         ParkingLotUtils::LicensePlate licensePlate,
         ParkingLotUtils::ParkingSpot &parkingSpot) const {
     return VEHICLE_ALREADY_PARKED;
 }
 
-/**
- * Remove a vehicle from the parking lot
- * @param licensePlate
- * @param exitTime
- * @return
- */
 ParkingLotUtils::ParkingResult
 MtmParkingLot::ParkingLot::exitParking(
         ParkingLotUtils::LicensePlate licensePlate,
@@ -61,21 +35,11 @@ MtmParkingLot::ParkingLot::exitParking(
     return VEHICLE_ALREADY_PARKED;
 }
 
-/**
- * Summon an inspector to check vehicles in the parking lot
- * @param inspectionTime
- */
 void MtmParkingLot::ParkingLot::inspectParkingLot(
         ParkingLotUtils::Time inspectionTime) {
 
 }
 
-/**
- * Print the contents of the parking lot object according to specification
- * @param os
- * @param parkingLot
- * @return
- */
 std::ostream
 &MtmParkingLot::operator<<(std::ostream &os,
                            const MtmParkingLot::ParkingLot &parkingLot) {
